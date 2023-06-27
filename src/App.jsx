618b9914934038dbb2a16a2d9a07a63bfefedb3e
Carenjot <carenjot.aurelien@hotmail.fr>
2023-06-27 17:17:47 +0200

    add body and legs

--- src/App.jsx
+++ src/App.jsx
@@ -32,16 +32,41 @@ function App() {
     rightPupil.position.set(15, 8, -5);
 
     const geometryNose = new THREE.ConeGeometry(5, 20, 32);
-    const materialNose = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    const materialNose = new THREE.MeshBasicMaterial({ color: 0xff8800 });
     const nose = new THREE.Mesh(geometryNose, materialNose);
-
-    const nose2 = new THREE.Mesh(geometryNose, materialNose);
 
     nose.position.set(18, 0, 0);
     nose.rotation.set(0, 0, -Math.PI / 2);
 
+    const nose2 = new THREE.Mesh(geometryNose, materialNose);
+
     nose2.position.set(18, 0, 0);
     nose2.rotation.set(0, 0, -Math.PI / 2 - 0.05);
+
+    const points = [];
+    for (let i = 0; i < 10; i++) {
+      points.push(new THREE.Vector2(Math.sin(i * 0.2) * 10 + 5, (i - 5) * 2));
+    }
+    const geometryBody = new THREE.LatheGeometry(points);
+    const lathe = new THREE.Mesh(geometryBody, material);
+
+    lathe.position.set(0, -8, 0);
+    lathe.rotation.set(0, 0, Math.PI);
+
+    const lathe2 = new THREE.Mesh(geometryBody, material);
+
+    lathe2.position.set(-2.5, -17.5, 0);
+
+    lathe2.rotation.set(0, 0, -0.4);
+
+    const geometryLeg = new THREE.CylinderGeometry(1, 3, 20, 4);
+    const leg = new THREE.Mesh(geometryLeg, materialNose);
+
+    leg.position.set(0, -30, 5);
+
+    const leg2 = new THREE.Mesh(geometryLeg, materialNose);
+
+    leg2.position.set(0, -30, -5);
 
     test.scene.add(
       sphere,
@@ -50,7 +75,11 @@ function App() {
       leftPupil,
       rightPupil,
       nose,
-      nose2
+      nose2,
+      lathe,
+      lathe2,
+      leg,
+      leg2
     );
   }, []);
 
